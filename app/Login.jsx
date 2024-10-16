b51f65cb0ff1e02d3ba000a92bf36b039fd77918
andrewliu6868 <andrewliu6867@gmail.com>
2024-10-16 12:51:21 -0700

    Fixed styling on welcome page

--- app/Login.jsx
+++ app/Login.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, StatusBar} from 'react-native'
+import { View, Text, StyleSheet, StatusBar, Alert} from 'react-native'
 import React from 'react'
 import { theme } from '../style/theme' 
 import ScreenWrap from '../components/ScreenWrap'
@@ -11,12 +11,22 @@ import { widthP, heightP } from '../style/deviceSpecs'
 import CustomInput from '../components/CustomInput'
 import LoadIndicator from '../components/LoadIndicator'
 import { useRef, useState } from 'react'
+import Button from '../components/Button'
 
 export default function Login() {
     const [loading, setLoading] = useState(false);
     const router = useRouter();
     const emailRef = useRef("");
     const passRef = useRef("");
+
+    const onSubmit = async() =>{
+        // check if all fields are filled in
+        if(!emailRef.current || !passRef.current){
+            Alert.alert('Login', 'Please fill in all fields!');
+            return;
+        }
+
+    }
   return (
     <ScreenWrap>
         <StatusBar style="dark"/>
@@ -28,6 +38,8 @@ export default function Login() {
             <View style={styles.spacing}>
                 <CustomInput icon = {<Email strokeWidth = {0.75} iconColor={theme.colors.gray} />} placeholder = "Enter your email" onChangeText={()=>emailRef.current}/>
                 <CustomInput icon = {<Password strokeWidth = {0.75} iconColor={theme.colors.gray}/>} placeholder = "Enter your password" onChangeText={()=> passRef.current}/>
+                <Text style={styles.noPassword}> Can't remember your password?</Text>
+                <Button titleText="Submit" loading={loading} onPress={onSubmit}/>
             </View>
         </View>
     </ScreenWrap>
@@ -47,6 +59,12 @@ const styles = StyleSheet.create({
     },
     spacing: {
         gap: 25,
+    },
+    noPassword: {
+        flexDirection: 'row',
+        fontSize: heightP(1.5),
+        color: theme.colors.text,
+        justifyContent: 'flex-end'
     }
 
 })
