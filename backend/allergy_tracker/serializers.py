b0c10ecfe2a810e7c22c0535fd492b09e3045866
muhammed <muhammed.aaliyan101@gmail.com>
2024-10-22 23:59:22 -0400

    set up docker for django server and postgres db

--- backend/allergy_tracker/serializers.py
+++ backend/allergy_tracker/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import AllergenSpecies, Medication, UserProfile, SymptomTracking, PollenData
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.models import User
 
 class AllergenSpeciesSerializer(serializers.ModelSerializer):
@@ -13,13 +15,31 @@ class PollenDataSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}} # ensure it's write only
-        
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
         return user
     
 class MedicationSerializer(serializers.ModelSerializer):
--- backend/allergy_tracker/views.py
+++ backend/allergy_tracker/views.py
@@ -4,13 +4,19 @@ from .serializers import UserSerializer, UserProfileSerializer, AllergenSpeciesS
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
 
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
-    # generic user creation inherited from CreateAPIView
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
     
