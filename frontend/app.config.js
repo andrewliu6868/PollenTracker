0bba0ef024b4b496cbcd6a0db7c78d5b3723bce5
andrewliu6868 <andrewliu6867@gmail.com>
2024-11-03 22:58:44 -0800

    linked google apis to app, loaded ios and android apis

---
+++ frontend/app.config.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+
+export default {
+    expo: {
+      name : "allergy-tracker",
+      slug: "allergy-tracker",
+      version : "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      },
+      ios: {
+        config:{
+            googleMapsApiKey: process.env.IOS_API_KEY, 
+        },
+        supportsTablet: true
+      },
+      android: {
+        config:{
+            googleMapsApiKey: process.env.ANDROID_API_KEY, 
+        },
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#ffffff"
+        }
+      },
+      web: {
+        favicon: "./assets/favicon.png"
+      },
+    },
+  };
