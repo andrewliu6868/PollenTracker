7cbf3c0a06e6759c81be7076b68f802dec28437e
andrewliu6868 <andrewliu6867@gmail.com>
2024-11-09 11:22:17 -0800

    Medication Reminder Page base functionality done with minimal styling

--- frontend/app/Medication.jsx
+++ frontend/app/Medication.jsx
@@ -8,18 +8,50 @@ import Button from '../components/Button';
 import React, {useState} from 'react';
 import {DataTable} from 'react-native-paper'
 import { heightP, widthP } from '../style/deviceSpecs';
+import {AddMedication} from './AddMedication.jsx'
 
 export default function Medication(){
     const router = useRouter();
     const [loading, setLoading] = useState(false);
+    const [meds, setMeds] = useState([]);
+    const [isAddModalVisible, setIsAddModalVisibile] = useState(false);
+
+    const addNewMedication = (med) => {
+        setMeds([...meds, {id:meds.length + 1, ...med}]);
+    }
 
     return (
         <ScreenWrap>
             <View style={styles.container}>
                 <TopBar title = "Medication"></TopBar>
-                <DataTable />
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>ID</DataTable.Title>
+                        <DataTable.Title>Medication</DataTable.Title>
+                        <DataTable.Title>Description</DataTable.Title>
+                        <DataTable.Title>Reminder Time</DataTable.Title>
+                    </DataTable.Header>
+                    <FlatList
+                        data={meds} 
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem ={({item}) => (
+                            <DataTable.Row>
+                                <DataTable.Cell>{item.id}</DataTable.Cell>
+                                <DataTable.Cell>{item.name}</DataTable.Cell>
+                                <DataTable.Cell>{item.time}</DataTable.Cell>
+                            </DataTable.Row>
+                        )}
+                    />
+                </DataTable>
 
-                <Button text="Add New Medication" loading={loading} onPress={() => {router.push('/Medication')}}/>
+                <Button text="Add New Medication" loading={loading} onPress={() => setIsAddModalVisibile(true)}/>
+
+                <AddMedication
+                    isVisible={isAddModalVisible}
+                    onClose={() => setIsAddModalVisibile(false)}
+                    onAddMedication ={addNewMedication}
+                />
+
             </View>
 
         </ScreenWrap>
